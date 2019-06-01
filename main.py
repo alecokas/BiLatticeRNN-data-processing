535cf27b0df83fbf41783a2dc4dfbace07f27fdc
alecokas <alecokastanos@gmail.com>
2019-06-01 14:28:30 +0100

    Setting up preprocessing

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 
 
 def parse_arguments(args_to_parse):
+    return None
     """ Parse the command line arguments.
 
         Arguments:
@@ -38,12 +39,26 @@ def unzip(input_dir, target_directory):
             else:
                 raise Exception('The target file must have the .gz extension.')
 
+def compile_lattice_list(lst_destination,
+                         base_directory='/home/dawna/babel/BABEL_OP3_404/releaseB/exp-graphemic-ar527-v3/J2/decode-ibmseg-fcomb/test/'):
+    subsets = ['dev', 'eval']
+    for subset in subsets:
+        print(os.path.join(base_directory,subset))
+        speaker_dirs = next(os.walk(os.path.join(base_directory,subset)))[1]
+        for speaker_dir in speaker_dirs:
+            raw_lattice_dir = os.path.join(speaker_dir, '/decode/rescore/tg_20.0_0.0/rescore/wlat_20.0_0.0/rescore/plat_20.0_0.0/lattices')
+            raw_lattice_list = next(os.walk(raw_lattice_dir))[2]
+            print(raw_lattice_list)
+    
+
 def main(args):
     """ Primary entry point for the script. """
-    unzip(
-        input_dir=args.input_dir,
-        target_directory=args.output_dir
-    )
+    # unzip(
+    #     input_dir=args.input_dir,
+    #     target_directory=args.output_dir
+    # )
+    compile_lattice_list(None)
+
 
 
 if __name__ == '__main__':

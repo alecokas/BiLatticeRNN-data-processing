14dc9ce3fd5fe32736655589b6692b9ad3b4beff
alecokas <alecokastanos@gmail.com>
2019-06-01 21:14:20 +0100

    Minor fix

--- main.py
+++ main.py
@@ -43,10 +43,10 @@ def compile_lattice_list(lst_destination,
                          base_directory='/home/dawna/babel/BABEL_OP3_404/releaseB/exp-graphemic-ar527-v3/J2/decode-ibmseg-fcomb/test/'):
     subsets = ['dev', 'eval']
     for subset in subsets:
-        print(os.path.join(base_directory,subset))
-        speaker_dirs = next(os.walk(os.path.join(base_directory,subset)))[1]
+        subset_dir = os.path.join(base_directory, subset)
+        speaker_dirs = next(os.walk(os.path.join(base_directory, subset)))[1]
         for speaker_dir in speaker_dirs:
-            raw_lattice_dir = os.path.join(speaker_dir, '/decode/rescore/tg_20.0_0.0/rescore/wlat_20.0_0.0/rescore/plat_20.0_0.0/lattices')
+            raw_lattice_dir = os.path.join(subset_dir, speaker_dir, 'decode/rescore/tg_20.0_0.0/rescore/wlat_20.0_0.0/rescore/plat_20.0_0.0/lattices')
             raw_lattice_list = next(os.walk(raw_lattice_dir))[2]
             print(raw_lattice_list)
     

105b13b84b03fb9d7f2443fff5aeda02f646c182
alecokas <alecokastanos@gmail.com>
2019-06-03 13:56:44 +0100

    strip phones

--- preprocess_lattices.py
+++ preprocess_lattices.py
@@ -8,10 +8,13 @@ import argparse
 from itertools import repeat
 from multiprocessing import Pool
 import numpy as np
+import re
 from toposort import toposort_flatten
 
 import utils
 from posterior import Dag
+
+EMBEDDING_LENGTH = 4
 
 def read_lattice(lattice_path):
     """Read HTK lattices.
@@ -62,8 +65,9 @@ def read_lattice(lattice_path):
             child = int(line[2].split('=')[1])
             am_score = float(line[3].split('=')[1])
             lm_score = float(line[4].split('=')[1])
-            post = float(line[5].split('=')[1])
-            edges.append([parent, child, am_score, lm_score, post])
+            # post = float(line[5].split('=')[1])
+            grapheme_info = get_grapheme(line[4].split('=')[1])
+            edges.append([parent, child, am_score, lm_score] + grapheme_info)
             if child not in dependency:
                 dependency[child] = {parent}
                 child_2_parent[child] = {parent: edge_id}
@@ -75,6 +79,55 @@ def read_lattice(lattice_path):
             else:
                 parent_2_child[parent][child] = edge_id
     return nodes, edges, dependency, child_2_parent, parent_2_child
+
+def get_grapheme(raw_input):
+    token_durs = []
+    token_list = []
+    subword_list = raw_input.split(':')
+    for subword_info in subword_list:
+        subword, subword_dur = subword_info.split(',')
+        token_durs = token_durs + float(subword_dur)
+        token_list = token_list + strip_phone(subword, 1, False)
+    print(token_list)
+
+
+def strip_phone(phone_info, phone_context_width, incl_posn_info):
+    """ Strip phones of context and optionally the location indicator
+
+        Arguments:
+            phone_info: String with the full phone context information and location indicators.
+            phone_context_width: The phone context width as an integer
+            incl_posn_info: A boolean indicator for whether or not to include the phone position information (^I, ^M, ^F)
+    """
+    if phone_context_width > 3:
+        raise Exception('The phone context width cannot be greater than 3.')
+
+    itemised_phone_info = re.split(r'\+|\-', phone_info)
+    if len(itemised_phone_info) == 1:
+        return itemised_phone_info[0] if incl_posn_info else remove_location_indicator(itemised_phone_info[0])
+    elif len(itemised_phone_info) == 3:
+        if phone_context_width > 1:
+            # Assume that if the context is 2 (bigram), we want the include the preceding phone
+            stop = phone_context_width
+            return itemised_phone_info[:stop] if incl_posn_info else remove_location_indicator(itemised_phone_info[:stop])
+        else:
+            return itemised_phone_info[1] if incl_posn_info else remove_location_indicator(itemised_phone_info[1])
+    else:
+        raise Exception('The phone length should be 1 or 3, but found {}'.format(len(itemised_phone_info)))
+
+def remove_location_indicator(phone_with_location):
+    """ Strip location indicators from a string or strings within a list and return the result as a string
+
+        Arguments:
+            phone_with_location: Either a string or list containing the raw phone with location indicators.
+    """
+    if isinstance(phone_with_location, list):
+        clean_phone_list = []
+        for phone in phone_with_location:
+            clean_phone_list.append(phone.split('^')[0])
+        return ' '.join(clean_phone_list)
+    else:
+        return phone_with_location.split('^')[0]
 
 # def arc_posterior(htk_file):
 #     """Get arc posterior from htk file."""
@@ -113,8 +166,8 @@ def process_one_lattice(lattice_path, dst_dir, wordvec):
             topo_order = toposort_flatten(dependency)
             # posterior = arc_posterior(lattice_path)
             # for each edge, the information contains
-            # [wordvec(50), duration(1), AM(1), LM(1), arc_posterior(1)]
-            edge_data = np.empty((len(edges), 50 + 1 + 1 + 1 + 1))
+            # [EMBEDDING_LENGTH, duration(1), AM(1), LM(1), arc_posterior(1)]
+            edge_data = np.empty((len(edges), EMBEDDING_LENGTH + 1 + 1 + 1 + 1))
             ignore = []
             for i, edge in enumerate(edges):
                 start_node = edge[0]

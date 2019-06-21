5d5e3d4fde7d68b0611ed002ff4e58050d0f28c0
alecokas <alecokastanos@gmail.com>
2019-06-21 17:23:53 +0100

    Modify CLI

--- one_lattice_per_speaker.py
+++ one_lattice_per_speaker.py
@@ -8,9 +8,6 @@
 import argparse
 
 
-NUM_LATTICES_PER_SPEAKER = 10
-
-
 def get_list(path):
     with open(path, 'r') as input_file:
         return input_file.readlines()
@@ -21,11 +18,19 @@ def save_list_to_file(list_to_save, dst_name):
             target_file.write('{}'.format(item))
 
 def main():
-    parser = argparse.ArgumentParser(description='Read in a txt file containing paths to lattices (*.npz) and extract one path for each speaker')
-    parser.add_argument('-f', '--file-to-process', required=True, type=str,
-                        help='The path to the txt file containing the dataset from which to extract file paths')
-    parser.add_argument('-d', '--dst-name', required=True, type=str,
-                        help='The output file for the subset list')
+    parser = argparse.ArgumentParser(description='Read in a txt file containing paths to lattices (*.npz) and extract N paths for each speaker')
+    parser.add_argument(
+        '-f', '--file-to-process', required=True, type=str,
+        help='The path to the txt file containing the dataset from which to extract file paths'
+    )
+    parser.add_argument(
+        '-d', '--dst-name', required=True, type=str,
+        help='The output file for the subset list'
+    )
+    parser.add_argument(
+        '-n', '--num-lats-per-speaker', default=10, type=int,
+        help='The number of paths to extract from each speaker'
+    )
     args = parser.parse_args()
 
     input_file = args.file_to_process
@@ -39,7 +44,7 @@ def main():
             speaker_dict[speaker] = 1
         else:
             speaker_dict[speaker] += 1
-        if speaker_dict[speaker] <= NUM_LATTICES_PER_SPEAKER:
+        if speaker_dict[speaker] <= args.num_lats_per_speaker:
             subset_list.append('{}XXXXX{}'.format(speaker, call_id))
 
     save_list_to_file(subset_list, args.dst_name)

64d8d760b103ca67cf412da0be5537006b7bd0d9
alecokas <alecokastanos@gmail.com>
2019-07-01 23:08:32 +0100

    Script for exploring the processed lattices

---
+++ processed_lattice_exploration.py
@@ -0,0 +1,62 @@
+import argparse
+import os
+import numpy as np
+import sys
+
+
+def read_lattices_edges(dataset_dir):
+    """ For each lattice in the dataset, find the number of edges
+        and return a list with the edge count for each lattice.
+    """
+    edge_count_list = []
+    for root, _, names in os.walk(dataset_dir):
+        for name in names:
+            if name.endswith('.npz'):
+                lattice_path = os.path.join(root, name)
+                lattice = np.load(lattice_path)
+                edge_count_list.append(len(lattice['edge_data']))
+    return np.asarray(edge_count_list)
+
+
+def generate_statistics(edge_count_list):
+    """ Return a dictionary with some statistics """
+    stats_dict = {}
+    stats_dict['mean'] = np.mean(edge_count_list)
+    stats_dict['std'] = np.std(edge_count_list)
+    stats_dict['median'] = np.median(edge_count_list)
+    stats_dict['pmf'] = np.histogram(edge_count_list, bins=np.arange(np.max(edge_count_list) + 1), density=True)
+    return stats_dict
+
+
+def main(args):
+    """ Primary entry point for the script. """
+    dataset_dir = args.target_dir
+    if not os.path.isdir(dataset_dir):
+        raise Exception('{} is not a valid directory'.format(dataset_dir))
+
+    edge_count_list = read_lattices_edges(dataset_dir)
+    stats_dict = generate_statistics(edge_count_list)
+    print(stats_dict)
+
+
+def parse_arguments(args_to_parse):
+    """ Parse the command line arguments.
+
+        Arguments:
+            args_to_parse:
+    """
+    description = "Do some data exploration of processed HTK lattices"
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument(
+        '-i', '--target-dir', type=str,
+        help='Location of the dataset with the uncompressed lattice files (*.npz)'
+    )
+
+    args = parser.parse_args(args_to_parse)
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_arguments(sys.argv[1:])
+    main(args)

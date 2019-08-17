ed3787370551e59cd343d6a278a1f267b74ad966
alecokas <alecokastanos@gmail.com>
2019-08-17 22:46:33 +0100

    Refactor file structure and rename STM to CTM as it should be

--- data-processing-scripts/split_ctm.py
+++ data-processing-scripts/split_ctm.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Script for spliting `.stm` files according to their names."""
+"""Script for spliting CTM (`.stm`) files according to their names."""
 
 import argparse
 import numpy as np
@@ -35,8 +35,8 @@ def get_segments(file_name):
     assert len(segments) == len(num_lines), "%i != %i" %(len(segments), len(num_lines))
     return segments, num_lines
 
-def split_stm(file_name, segments, num_lines, dst_dir):
-    """Split stm into multiple smaller ones by segments."""
+def split_ctm(file_name, segments, num_lines, dst_dir):
+    """Split ctm into multiple smaller ones by segments."""
     with open(file_name, 'r') as file_in:
         counter = 0
         for segment, num_line in zip(segments, num_lines):
@@ -63,34 +63,34 @@ def num_lines_in_file(file_name):
     return line_idx + 1
 
 def main():
-    """Main function for spliting `.stm` into `.npz` segnemts."""
+    """Main function for spliting CTM files (`.stm`) into `.npz` segnemts."""
     parser = argparse.ArgumentParser(
-        description='Split the two *.stm alignment files into the respective lattice file directories'
+        description='Split the two CTM files (*.stm) (alignment files) into the respective lattice file directories'
     )
     parser.add_argument(
         '-o', '--output-dir', type=str, required=True,
         help='Destination directory for the split output files'
     )
     parser.add_argument(
-        '-dev', '--dev-stm', type=str, required=True,
+        '-dev', '--dev-ctm', type=str, required=True,
         help='Path to the file containing the dev set train.stm alignment'
     )
     parser.add_argument(
-        '-eval', '--eval-stm', type=str, required=True,
+        '-eval', '--eval-ctm', type=str, required=True,
         help='Path to the file containing the eval set train.stm alignment'
     )
     args = parser.parse_args()
 
-    if os.path.isfile(args.dev_stm) and os.path.isfile(args.eval_stm):
-        stm_files = [args.dev_stm, args.eval_stm]
+    if os.path.isfile(args.dev_ctm) and os.path.isfile(args.eval_ctm):
+        ctm_files = [args.dev_ctm, args.eval_ctm]
     else:
-        raise FileNotFoundError('Please ensure that both {} and {} are valid files'.format(args.dev_stm, args.eval_stm))
+        raise FileNotFoundError('Please ensure that both {} and {} are valid files'.format(args.dev_ctm, args.eval_ctm))
 
     utils.mkdir(args.output_dir)
 
-    for stm_file in stm_files:
-        segments, num_lines = get_segments(stm_file)
-        split_stm(stm_file, segments, num_lines, args.output_dir)
+    for ctm_file in ctm_files:
+        segments, num_lines = get_segments(ctm_file)
+        split_ctm(ctm_file, segments, num_lines, args.output_dir)
 
 if __name__ == '__main__':
     main()

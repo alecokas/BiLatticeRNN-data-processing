4c8e7a6c0b71aa2d00e408b3cdc90db770f835fb
alecokas <alecokastanos@gmail.com>
2019-10-19 18:24:51 +0100

    Add ability to save all files which do not exist for a list in a file

--- exploration-scripts/find_missing_files.py
+++ exploration-scripts/find_missing_files.py
@@ -1,9 +1,23 @@
 import argparse
+import os
 import sys
 
 sys.path.append('../data-processing-scripts/')
 from enrich_confusion_network import set_of_processed_file_names
 
+
+def find_non_existent_files(path_list, remove_extension=True, extension='.npz'):
+    """ Return a list of all file names which belong to files which do not exist """
+    missing_files = []
+    for path_name in path_list:
+        if not os.path.isfile(path_name):
+            if remove_extension:
+                name = path_name.split('/')[-1]
+                name = name[:-len(extension)]
+                missing_files.append(name)
+            else:
+                missing_files.append(path_name)
+    return missing_files
 
 def files_to_search_for(file_name):
     with open(file_name, 'r') as list_file:
@@ -11,17 +25,20 @@ def files_to_search_for(file_name):
         return [elem.strip() for elem in content_list]
 
 def main(args):
-    files_in_dir = set_of_processed_file_names(
-        directory_to_search=args.directory_to_search,
-        remove_extension=True
-    )
+    """ Primary entry point for script which finds missing files """
+    if args.full_paths:
+        find_non_existent_files(args.list_file)
+    else:
+        files_in_dir = set_of_processed_file_names(
+            directory_to_search=args.directory_to_search,
+            remove_extension=True
+        )
+        query_files = files_to_search_for(args.list_file)
 
-    query_files = files_to_search_for(args.list_file)
-
-    missing_files = []
-    for query_file in query_files:
-        if not query_file in files_in_dir:
-            missing_files.append(query_file)
+        missing_files = []
+        for query_file in query_files:
+            if not query_file in files_in_dir:
+                missing_files.append(query_file)
 
     with open(args.output_file, 'w') as out_file:
         out_file.write('\n'.join(missing_files))
@@ -34,7 +51,7 @@ def parse_arguments(args_to_parse):
 
     parser.add_argument(
         '-l', '--list-file', type=str, required=True,
-        help='A file with a list of names to check (*.lst).'
+        help='A file with a list of names to check (*.lst) or (*.npz)'
     )
     parser.add_argument(
         '-d', '--directory-to-search', type=str, required=True,
@@ -43,6 +60,10 @@ def parse_arguments(args_to_parse):
     parser.add_argument(
         '-o', '--output-file', type=str, required=True,
         help='File to save the names of all missing files'
+    )
+    parser.add_argument(
+        '--full-paths', dest='full_paths', action='store_true', default=False,
+        help='Flag to indicate that the list file contains the full paths to the search files'
     )
     args = parser.parse_args(args_to_parse)
     return args
